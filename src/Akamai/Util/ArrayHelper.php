b3204456f71854e6d3daf58d124bca35d09a6782
Cerlin <cerlin@linkstreet.in>
2016-08-01 15:10:36 +0530

    Added array helper to help with flattening the array

---
+++ src/Akamai/Util/ArrayHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Akamai\Util;
+
+class ArrayHelper
+{
+
+    /**
+     * Flatten a multi-dimensional array into a single level.
+     *
+     * @param  array  $array
+     * @param  int  $depth
+     * @return array
+     */
+    public static function flatten($array, $depth = INF)
+    {
+        $result = [];
+
+        foreach ($array as $item) {
+            // $item = $item instanceof Collection ? $item->all() : $item;
+
+            if (is_array($item)) {
+                if ($depth === 1) {
+                    $result = array_merge($result, $item);
+                    continue;
+                }
+
+                $result = array_merge($result, static::flatten($item, $depth - 1));
+                continue;
+            }
+
+            $result[] = $item;
+        }
+
+        return $result;
+    }
+}
